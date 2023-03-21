a0a834679fe61fa0c41f05cc02fa1a5d75769890
Gabriel97Project <gabriel97tomaz@hotmail.com>
2023-03-21 18:02:35 -0300

    adicionando mascara ao input

--- project.js
+++ project.js
@@ -3,30 +3,68 @@ let valueArray = JSON.parse(localStorage.getItem('arraySalvo')) || []
 let myInputValues = JSON.parse(localStorage.getItem('valorDosInputs')) || [];
 let result = JSON.parse(localStorage.getItem('resultados')) || 0;
 let createPositiveOrNegativeResult;
+console.log(valueArray)
+console.log(result)
+let valueCampMask = document.getElementById('merchandiseValue');
+function valueInputRestrict(eventoDown) {
+    if (
+        '0123456789'.indexOf(eventoDown.key) == -1 &&
+        eventoDown.key != 'Backspace' &&
+        eventoDown.key != 'Alt' &&
+        eventoDown.key != 'Ctrl'
+    ) {
+        eventoDown.preventDefault();
+    }
+
+    /*  console.log(eventoDown) */
+}
+function valueMask(eventoUp) {
+    console.log(eventoUp);
+    let valueMask = eventoUp.target.value;
+
+    valueMask = valueMask.replaceAll('.', '').replace(',', '').replaceAll('R$', '');
+
+    valueMask = valueMask.replace(/([0-9]{2})$/gi, '.$1');
+  
+    valueMask = parseFloat(valueMask);
+
+    eventoUp.target.value = valueMask.toLocaleString('pt-BR');
+    console.log(valueMask, "babababab")
+}
+
+
+
+    const formatter = new Intl.NumberFormat('pt-BR',{
+        style: "currency",
+        currency: "BRL",
+        
+    });
+    
+let formatedResult = formatter.format(result);
 
 function myFormValues(e) {
     e.preventDefault();
-    let numbersVerify = /[^0-9]+/g
-    let stringsVerify = /[^a-zA-Z]+/g
-    if (stringsVerify.test(e.target.elements["inputMerchandiseNameStyle"].value)) {
-        console.log("peguei o erro string")
-        alert("Somente letras são aceitas no campo de mercadoria!")
-        return false
-    }
-    if (numbersVerify.test(e.target.elements["inputMerchandiseValueStyle"].value)) {
-        console.log("peguei o erro")
-        alert("Somente números são aceitos no campo de valor!")
-        return false
-    }
+    /* let numbersVerify = /[^0-9]+/g */
+    /*   let stringsVerify = /[^a-zA-Z]+/g */
+    /*   if (stringsVerify.test(e.target.elements["inputMerchandiseNameStyle"].value)) {
+          console.log("peguei o erro string")
+          alert("Somente letras são aceitas no campo de mercadoria!")
+          return false
+      } */
+    /*  if (numbersVerify.test(e.target.elements["inputMerchandiseValueStyle"].value)) {
+         console.log("peguei o erro")
+         alert("Somente números são aceitos no campo de valor!")
+         return false
+     } */
 
-    let seleçãoCompraOuVenda = document.getElementById('selectCampStyle').value;
+    let selecaoCompraOuVenda = document.getElementById('selectCampStyle').value;
     let nomeDaMercadoria = document.getElementById('inputMerchandiseNameStyle').value;
     let valorDaMercadoria = document.getElementById('inputMerchandiseValueStyle').value;
 
     valueArray.push(valorDaMercadoria);
     localStorage.setItem('arraySalvo', JSON.stringify(valueArray))
 
-    myInputValues.push({ seleção: seleçãoCompraOuVenda, nomeDaMercadoria, valorDaMercadoria });
+    myInputValues.push({ selecaoCompraOuVenda, nomeDaMercadoria, valorDaMercadoria });
     localStorage.setItem('valorDosInputs', JSON.stringify(myInputValues));
 
     let trCreate = document.createElement('tr');
@@ -37,25 +75,26 @@ function myFormValues(e) {
     tdCreateTwo.className = 'tdCreateTwoStyle'
     let tdCreateThree = trCreate.appendChild(document.createElement('td'));
     tdCreateThree.className = 'tdCreateThreeStyle'
-    tdCreateOne.innerHTML = seleçãoCompraOuVenda;
+    tdCreateOne.innerHTML = selecaoCompraOuVenda;
     tdCreateTwo.innerHTML = nomeDaMercadoria;
-    tdCreateThree.innerHTML = valorDaMercadoria;
+    tdCreateThree.innerHTML = `R$ ${valorDaMercadoria}`;
     document.getElementById('tableBodyStyle').appendChild(trCreate);
 
     let valueArrayLastNumber = valueArray[valueArray.length - 1];
-    if(seleçãoCompraOuVenda == "+") {
-        result = result + parseInt(valueArrayLastNumber);
-        document.getElementById('dynamicTotalStyle').innerHTML = `R$ ${result}`;
+    if (selecaoCompraOuVenda == "+") {
+        result = result + parseFloat(valueArrayLastNumber.replaceAll('.', '').replace(',', '.'));
+       
         localStorage.setItem('resultados', (result));
     };
-    if(seleçãoCompraOuVenda == "-") {
-        result = result - parseInt(valueArrayLastNumber);
-        document.getElementById('dynamicTotalStyle').innerHTML = `R$ ${result}`;
+    if (selecaoCompraOuVenda == "-") {
+        result = result - parseFloat(valueArrayLastNumber.replaceAll('.', '').replace(',', '.'));
+       
         localStorage.setItem('resultados', (result));
     };
+    document.getElementById('dynamicTotalStyle').innerHTML = formatter.format(result);
 
     let positiveAndNegativeId = document.getElementById("positiveAndNegativeResultsId");
-    if(!positiveAndNegativeId){
+    if (!positiveAndNegativeId) {
         createPositiveOrNegativeResult = document.createElement('p');
         document.getElementById('tableRowTotalResultStyle').appendChild(createPositiveOrNegativeResult);
         createPositiveOrNegativeResult.setAttribute("id", "positiveAndNegativeResultsId");
@@ -65,13 +104,13 @@ function myFormValues(e) {
         criado nosso if vai detect-lo, dessa forma ele nao vai mais criar o elemento... caso
         isso nao fosse feito, toda vez que a função rodasse o elemento html seria criado novamente.*/
     };
-    if(result == 0){
+    if (result == 0) {
         document.getElementById('positiveAndNegativeResultsId').innerHTML = "";
     };
-    if(result > 0){
+    if (result > 0) {
         document.getElementById('positiveAndNegativeResultsId').innerHTML = "[LUCRO]";
     };
-    if(result < 0){
+    if (result < 0) {
         document.getElementById('positiveAndNegativeResultsId').innerHTML = "[PREJUIZO]";
     };
 };
--- projectGetStorageData.js
+++ projectGetStorageData.js
@@ -1,59 +1,58 @@
 
-
 function loadScreenGetStorage() {
     let loadGetStorage = document.getElementById('tableBodyStyle');
     myInputValues.map((inputValuesUnit) => {
         var trCreate = document.createElement('tr');
         trCreate.className = 'trCreateDinamicStyle'
-        let createTableStorageLoadOne = trCreate.appendChild(document.createElement('td'))
+        trCreate.innerHTML = `
+            <td class = "tdCreateOneStyle">${inputValuesUnit.selecaoCompraOuVenda}</td>
+            <td class = "tdCreateTwoStyle">${inputValuesUnit.nomeDaMercadoria}</td>
+            <td class= "tdCreateThreeStyle">${inputValuesUnit.valorDaMercadoria}</td>
+        ` 
+        loadGetStorage.appendChild(trCreate)
+        
+        /* let createTableStorageLoadOne = trCreate.appendChild(document.createElement('td'))
         createTableStorageLoadOne.className = "tdCreateOneStyle"
         let createTableStorageLoadTwo = trCreate.appendChild(document.createElement('td'))
         createTableStorageLoadTwo.className = "tdCreateTwoStyle"
         let createTableStorageLoadThree = trCreate.appendChild(document.createElement('td'))
         createTableStorageLoadThree.className = "tdCreateThreeStyle"
-
-
-        loadGetStorage.appendChild(trCreate)
-        createTableStorageLoadOne.innerHTML = inputValuesUnit.seleção;
-        createTableStorageLoadTwo.innerHTML = inputValuesUnit.nomeDaMercadoria;
-        createTableStorageLoadThree.innerHTML = inputValuesUnit.valorDaMercadoria;
-        console.log(inputValuesUnit.valorDaMercadoria, "inpuuuuuuts")
+ */
+/* 
+       
+        createTableStorageLoadOne.innerHTML = 
+        createTableStorageLoadTwo.innerHTML = 
+        createTableStorageLoadThree.innerHTML =
+        console.log(inputValuesUnit.valorDaMercadoria, "inpuuuuuuts") */
     })
    /*  let positiveValue = "[LUCRO]";
     let negativeValue = "[PREJUIZO]";
     let valueUndefined = ""; */
 
-    document.getElementById('dynamicTotalStyle').innerHTML = `R$ ${result}`;
-/* 
+    document.getElementById('dynamicTotalStyle').innerHTML = formatter.format(result);
+
     let positiveAndNegativeId = document.getElementById("positiveAndNegativeResultsId");
     if (!positiveAndNegativeId) {
         let = createPositiveOrNegativeResult = document.createElement('p');
         document.getElementById('dynamicTotalStyle').appendChild(createPositiveOrNegativeResult);
         createPositiveOrNegativeResult.setAttribute("id", "positiveAndNegativeResultsId");
 
-    } 
+    }  
+
     
    if (result == 0) {
-        document.getElementById('positiveAndNegativeResultsId').innerHTML = valueUndefined;
+        document.getElementById('positiveAndNegativeResultsId').innerHTML = "";
 
     }
     if (result > 0) {
-        document.getElementById('positiveAndNegativeResultsId').innerHTML = positiveValue;
+        document.getElementById('positiveAndNegativeResultsId').innerHTML = "[LUCRO]";
 
     }
     if (result < 0) {
-        document.getElementById('positiveAndNegativeResultsId').innerHTML = negativeValue;
+        document.getElementById('positiveAndNegativeResultsId').innerHTML = "[PREJUÍZO]";
 
     } 
 
-    /*   if (result > 0) {
-          document.getElementById('positiveAndNegativeResultsId').innerHTML = positiveValue;
-  
-      }
-      if(result < 0){
-          document.getElementById('positiveAndNegativeResultsId').innerHTML = negativeValue;
-      }
-  */
 
 }
 function deleteAllStorageData(){
